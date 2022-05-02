f1b180a30938ecf64a58f8dd3da0da5d372b76db
RupakSapkota123 <rupaksapkota2000@gmail.com>
2022-05-02 10:52:49 +0545

    First test

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  // expect(h3.innerHTML).contain('hello there')
   const linkElement = screen.getByText("Our First Test");
   expect(linkElement).toBeInTheDocument();
 });
